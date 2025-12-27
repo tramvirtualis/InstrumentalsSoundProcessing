ab774dbca79642b2a42691423fa85ecdf654e03b
tramvirtualis <ngoctramngff4@gmail.com>
2025-12-28 00:04:35 +0700

    delete

--- Xử lý tiếng nói/InfomationWave2.py
+++
@@ -1,6 +0,0 @@
-import soundfile as sf
-
-data, samplerate = sf.read('trial.wav')
-print('Sample rate: ', samplerate)
-print('Audio data shape: ', data.shape)
-print('First 10 samples of audio data: ', data[:10])
